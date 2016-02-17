77090c6ac8a5613f21aae6818b02fccbe036ef64
Dominick Baier <dbaier@leastprivilege.com>
2016-02-17 10:34:50 +0100

    cleanup

--- IdentityModel.Uwp.OidcClient/OidcTokenManagerState.cs
+++ IdentityModel.Uwp.OidcClient/OidcTokenManagerState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdentityModel.Uwp.OidcClient
+﻿namespace IdentityModel.Uwp.OidcClient
 {
     internal class OidcTokenManagerState
     {
--- IdentityModel.Uwp.OidcClient/WebView/DisplayMode.cs
+++ IdentityModel.Uwp.OidcClient/WebView/DisplayMode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdentityModel.Uwp.OidcClient.WebView
+﻿namespace IdentityModel.Uwp.OidcClient.WebView
 {
     public enum DisplayMode
     {
